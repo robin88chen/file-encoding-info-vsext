f2f69095a552edc393b9e7acea2c964f7a57fe75
Lancelot Chen <jongbin.chen@gmail.com>
2019-04-13 11:20:46 +0800

    show all text files info, not just code files

--- FileEncodingInfo/EncodingInfoMarginFactory.cs
+++ FileEncodingInfo/EncodingInfoMarginFactory.cs
@@ -13,8 +13,8 @@ namespace FileEncodingInfo
     [Name(EncodingInfoMargin.MarginName)]
     [Order(After = PredefinedMarginNames.HorizontalScrollBar)]  // Ensure that the margin occurs below the horizontal scrollbar
     [MarginContainer(PredefinedMarginNames.Bottom)]             // Set the container to the bottom of the editor window
-    [ContentType("code")]                                       // Show this margin for all text-based types
-    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    [ContentType("text")]                                       // Show this margin for all text-based types
+    [TextViewRole(PredefinedTextViewRoles.Document)]
     internal sealed class EncodingInfoMarginFactory : IWpfTextViewMarginProvider
     {
         #region IWpfTextViewMarginProvider
